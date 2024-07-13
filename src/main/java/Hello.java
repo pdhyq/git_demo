e9d219fc3c5008443b765b036885aa4d06781afc
du-haoyi <2570571063@qq.com>
2024-07-13 11:34:51 +0800

    dev2-第一次提交

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("你好，打工人");
     }
 }
